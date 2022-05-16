44e92794706b73a39b0a649a67c01ca33e21e617
Maxim Krasotsky <krasotsky2003@gmail.com>
2022-05-16 15:42:35 +0300

    rebase models and create new migrations

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
 
 
 User = get_user_model()
@@ -22,8 +24,8 @@ class Product(models.Model):
     # MAX_RESOLUTION = (1200, 1200)
     # MAX_IMAGE_SIZE = 3145728
     
-    # class Meta:
-    #     abstract = True 
+    class Meta:
+        abstract = True 
     
     category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
     title = models.CharField(max_length=255, verbose_name='Наименование')
@@ -84,9 +86,9 @@ class CartProduct(models.Model):
     
     user = models.ForeignKey('Customer', verbose_name='Покупатель', on_delete=models.CASCADE)
     cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE, related_name='related_products')
-    product = models.ForeignKey(Product, verbose_name='Товар', on_delete=models.CASCADE)
-    # object_id = models.PositiveIntegerField()
-    # content_object = GenericForeignKey('content_type', 'object_id')
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
     qty = models.PositiveIntegerField(default=1)
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена')
     
@@ -116,12 +118,3 @@ class Customer(models.Model):
     def __str__(self):
         return f"Покупатель: {self.user.first_name} {self.user.last_name}"
     
-
-class Specifications(models.Model):
-    
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    name = models.CharField(max_length=255, verbose_name='Имя товара для характеристик')
-    
-    def __str__(self):
-        return f"Характеристики для товара: {self.name}"

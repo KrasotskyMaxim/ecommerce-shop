c2825d9f559e3b32cabaa0a36da052f2bb0e269e
Maxim Krasotsky <krasotsky2003@gmail.com>
2022-05-20 21:51:19 +0300

    create addting products to cart and actualizing cart

--- shop/mainapp/urls.py
+++ shop/mainapp/urls.py
@@ -1,12 +1,12 @@
-from lib2to3.pytree import Base
 from django.urls import path
 
-from .views import BaseView, ProductDetailView, CategoryDetailView, CartView
+from .views import *
 
 
 urlpatterns = [
     path('', BaseView.as_view(), name='base'),
     path('products/<str:ct_model>/<str:slug>/', ProductDetailView.as_view(), name='product_detail'),
     path('category/<str:slug>/', CategoryDetailView.as_view(), name='category_detail'),
-    path('cart/', CartView.as_view(), name='cart')
+    path('cart/', CartView.as_view(), name='cart'),
+    path('add-to-cart/<str:ct_model>/<str:slug>/', AddToCartView.as_view(), name='add_to_cart')
 ]
--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -1,28 +1,31 @@
 from django.shortcuts import render
+from django.contrib.contenttypes.models import ContentType
+from django.http import HttpResponseRedirect
 from django.views.generic import DetailView, View
 
 from .models import *
-from .mixins import CategoryDetailMixin
+from .mixins import CategoryDetailMixin, CartMixin
 
 # Create your views here.
 
-class BaseView(View):
+class BaseView(CartMixin, View):
     
     def get(self, request, *args, **kwrgs):
         categories = Category.objects.get_categories_for_dropdown()
         products = LatestProducts.objects.get_products_for_main_page(
             'notebook', 
             'smartphone',
-            # with_respect_to='notebook'
+            with_respect_to='notebook'
         )
         context = {
             'categories': categories,
-            'products': products
+            'products': products,
+            'cart': self.cart
         }
         return render(request, 'base.html', context)
 
 
-class ProductDetailView(CategoryDetailMixin, DetailView):
+class ProductDetailView(CartMixin, CategoryDetailMixin, DetailView):
     
     CT_MODEL_CLASS = {
         'notebook': Notebook,
@@ -38,8 +41,13 @@ class ProductDetailView(CategoryDetailMixin, DetailView):
     template_name = 'product_detail.html'
     slug_url_kwarg = 'slug'
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['ct_model'] = self.model._meta.model_name
+        return context
+    
 
-class CategoryDetailView(CategoryDetailMixin, DetailView):
+class CategoryDetailView(CartMixin, CategoryDetailMixin, DetailView):
     
     model = Category
     queryset = Category.objects.all()
@@ -47,15 +55,28 @@ class CategoryDetailView(CategoryDetailMixin, DetailView):
     template_name = 'category_detail.html'
     slug_url_kwarg = 'slug'
     
-    
-class CartView(View):
+
+class AddToCartView(CartMixin, View):
     
     def get(self, request, *args, **kwargs):
-        customer = Customer.objects.get(user=request.user)
-        cart = Cart.objects.get(owner=customer)
+        ct_model, product_slug = kwargs.get('ct_model'), kwargs.get('slug')
+        content_type = ContentType.objects.get(model=ct_model)
+        product = content_type.model_class().objects.get(slug=product_slug)
+        cart_product, created = CartProduct.objects.get_or_create(
+            user=self.cart.owner, cart=self.cart, content_type=content_type, object_id=product.id
+        ) 
+        if created:
+            self.cart.products.add(cart_product) 
+        self.cart.save()
+        return HttpResponseRedirect('/cart/')
+    
+    
+class CartView(CartMixin, View):
+    
+    def get(self, request, *args, **kwargs):
         categories = Category.objects.get_categories_for_dropdown()
         context = {
-            'cart': cart,
+            'cart': self.cart,
             'categories': categories
         }
         return render(request, 'cart.html', context)
